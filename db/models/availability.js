0d2a224c43fe3f81057d2f86562aad73d5b27738
Jacob Regan <jacobmregan0@gmail.com>
2017-06-05 15:48:44 -0500

    Added Availability Model

---
+++ db/models/availability.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const {STRING} = require('sequelize')
+
+module.exports = db => db.define('availability', {
+  date: STRING
+})
+
+module.exports.associations = (Availability, {Home}) => {
+  Availability.belongsToMany(Home)
+}
