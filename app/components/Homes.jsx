6714bf0b93802f726668436ab31324878d432a46
Jacob Regan <jacobmregan0@gmail.com>
2017-06-07 18:02:05 -0500

    Removes Add to Cart button from multiple homes view

--- app/components/Homes.jsx
+++ app/components/Homes.jsx
@@ -12,15 +12,14 @@ const Homes = (props) => {
         return (
           <div className="row list-group-item" key={ home.id }>
             <div className="row">
-              <div className="col-sm-12">
-                <img className="col-sm-3" src={home.imageUrl}/>
+              <div className="col-sm-3">
+                <img className="col-sm-12" src={home.imageUrl}/>
               </div>
               <div className="col-sm-9">
                 <h3><Link to={`/homes/${home.id}`}>{home.name}</Link></h3>
                 <h4>Price/Night: ${home.price}</h4>
                 <p>{home.excerpt}</p>
-                <button className = 'btn btn-primary' onClick = {() => {}} >Add to Cart</button>
-                <Link className = 'btn btn-default' to={`/homes/${home.id}`}>More Information</Link>
+                <Link className = 'btn btn-primary' to={`/homes/${home.id}`}>More Information</Link>
               </div>
             </div>
           </div>
