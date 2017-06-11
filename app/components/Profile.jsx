a0f98232f8c158269182a9e09128c6e52e8df4b4
Rob Tong <robtong21@gmail.com>
2017-06-11 02:01:51 -0500

    puts transactions in tabs; not working yet

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -5,7 +5,7 @@ import moment from 'moment'
 const SelectedUser= ({selected, userHomes, guest, host}) => {
   const user = selected
   return (
-    <div className="panel panel-default">
+    <div className="panel">
       <div className="col-lg-3 col-md-3 hidden-sm hidden-xs panel-body">
           <div className="media">
               <div>
@@ -31,76 +31,92 @@ const SelectedUser= ({selected, userHomes, guest, host}) => {
           </div>
       </div>
       <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12 panel-body">
-            <div className="panel panel-default">
+            <div className="panel with-nav-tabs">
+
+                <div className="panel-heading">
+                    <ul className="nav nav-tabs">
+                        <li className="active"><a href="#tab1default" data-toggle="tab">Upcoming Reservations</a></li>
+                        <li><a href="#tab2default" data-toggle="tab">Past Reservations</a></li>
+                        <li><Link to="#tab3default" data-toggle="tab">Transaction History as Host</Link></li>
+                    </ul>
+                </div>
                 <div className="panel-body">
-                    <h1>Your Reservations</h1>
-                    <div className="table table-striped table-hover">
-                        {guest.length ? guest.map(transaction => {
-                            return (
-                                <div key={transaction.id}>
-                                    <div className="order order-header row thead-inverse">
-                                        <div className="order-column">
-                                            <div><strong>Order placed</strong></div>
-                                            <div>{moment(transaction.created_at).format('LL')}</div>
+                    <div className="tab-content">
+                        <div className="tab-pane fade in active" id="tab1default">
+                            <h1>Your Upcoming Reservations</h1>
+                            <div className="table table-striped table-hover">
+                                {guest.length ? guest.map(transaction => {
+                                    return (
+                                        <div key={transaction.id}>
+                                            <div className="order order-header row thead-inverse">
+                                                <div className="order-column">
+                                                    <div><strong>Order placed</strong></div>
+                                                    <div>{moment(transaction.created_at).format('LL')}</div>
+                                                </div>
+                                                <div className="order-column">
+                                                    <div><strong>Total</strong></div>
+                                                    <div>${transaction.price * (moment(transaction.endDate).diff(moment(transaction.startDate), 'days'))}</div>
+                                                </div>
+                                                <div className="right">
+                                                    <div><strong>Order #</strong></div>
+                                                    <div className="center">{transaction.id}</div>
+                                                </div>
+                                            </div>
+                                            <div className="order order-body flex">
+                                                <div className="left"><img src={transaction.home.imageUrl} className="order-thumb"/></div>
+                                                <div className="no-flex">
+                                                    <div className="bottom-padding"><Link to="/homes/{transaction.home.id}" className="link">{transaction.home.name}</Link></div>
+                                                    <div className="bottom-padding">From <strong>{moment.utc(transaction.startDate).format('LL')}</strong> to <strong>{moment.utc(transaction.endDate).format('LL')}</strong></div>
+                                                    <div className="price">${transaction.price}</div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="order-column">
-                                            <div><strong>Total</strong></div>
-                                            <div>${transaction.price * (moment(transaction.endDate).diff(moment(transaction.startDate), 'days'))}</div>
+                                    )
+                                }) : <p>You have no upcoming reservations for other hosts' homes.</p>}
+                            </div>
+                        </div>
+                        <div className="tab-pane fade" id="tab2default">
+                        Default 2
+                        </div>
+                        <div className="tab-pane fade" id="tab3default">
+                            <div className="table table-striped table-hover">
+                                <h1>Transaction History as Host</h1>
+                                <div className="table table-striped table-hover">
+                                    {host.length ? host.map(transaction => {
+                                        console.log("host", host)
+                                        return (
+                                            <div key={transaction.id}>
+                                                <div className="order order-header row thead-inverse">
+                                                    <div className="order-column">
+                                                <div><strong>Order placed</strong></div>
+                                                    <div>{moment(transaction.created_at).format('LL')}</div>
+                                                </div>
+                                                <div className="order-column">
+                                                    <div><strong>Total</strong></div>
+                                                    <div>${transaction.price * (moment(transaction.endDate).diff(moment(transaction.startDate), 'days'))}</div>
+                                                </div>
+                                                <div className="order-column">
+                                                    <div><strong>Guest</strong></div>
+                                                    <div><a href={`mailto:${transaction.guest.email}`}>{transaction.guest.name}</a></div>
+                                                </div>
+                                                <div className="right">
+                                                    <div><strong>Order #</strong></div>
+                                                    <div className="center">{transaction.id}</div>
+                                                </div>
+                                            </div>
+                                            <div className="order order-body flex">
+                                                <div className="left"><img src={transaction.home.imageUrl} className="order-thumb"/></div>
+                                                <div className="no-flex">
+                                                    <div className="bottom-padding"><Link to="/homes/{transaction.home.id}" className="link">{transaction.home.name}</Link></div>
+                                                    <div className="bottom-padding">From <strong>{moment.utc(transaction.startDate).format('LL')}</strong> to <strong>{moment.utc(transaction.endDate).format('LL')}</strong></div>
+                                                    <div className="price">${transaction.price}</div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="right">
-                                            <div><strong>Order #</strong></div>
-                                            <div className="center">{transaction.id}</div>
-                                        </div>
-                                    </div>
-                                    <div className="order order-body flex">
-                                        <div className="left"><img src={transaction.home.imageUrl} className="order-thumb"/></div>
-                                        <div className="no-flex">
-                                            <div className="bottom-padding"><Link to="/homes/{transaction.home.id}" className="link">{transaction.home.name}</Link></div>
-                                            <div className="bottom-padding">From <strong>{moment.utc(transaction.startDate).format('LL')}</strong> to <strong>{moment.utc(transaction.endDate).format('LL')}</strong></div>
-                                            <div className="price">${transaction.price}</div>
-                                        </div>
-                                    </div>
+                                        )
+                                    }) : <p>No one has made reservations for your home yet.</p>}
                                 </div>
-                            )
-                        }) : <p>You have no reservations for other hosts' homes.</p>}
-                    </div>
-                    <hr/>
-                    <div className="table table-striped table-hover">
-                        <h1>Transaction History as Host</h1>
-                        <div className="table table-striped table-hover">
-                            {host.length ? host.map(transaction => {
-                                console.log("host", host)
-                                return (
-                                    <div key={transaction.id}>
-                                        <div className="order order-header row thead-inverse">
-                                            <div className="order-column">
-                                        <div><strong>Order placed</strong></div>
-                                            <div>{moment(transaction.created_at).format('LL')}</div>
-                                        </div>
-                                        <div className="order-column">
-                                            <div><strong>Total</strong></div>
-                                            <div>${transaction.price * (moment(transaction.endDate).diff(moment(transaction.startDate), 'days'))}</div>
-                                        </div>
-                                        <div className="order-column">
-                                            <div><strong>Guest</strong></div>
-                                            <div><a href={`mailto:${transaction.guest.email}`}>{transaction.guest.name}</a></div>
-                                        </div>
-                                        <div className="right">
-                                            <div><strong>Order #</strong></div>
-                                            <div className="center">{transaction.id}</div>
-                                        </div>
-                                    </div>
-                                    <div className="order order-body flex">
-                                        <div className="left"><img src={transaction.home.imageUrl} className="order-thumb"/></div>
-                                        <div className="no-flex">
-                                            <div className="bottom-padding"><Link to="/homes/{transaction.home.id}" className="link">{transaction.home.name}</Link></div>
-                                            <div className="bottom-padding">From <strong>{moment.utc(transaction.startDate).format('LL')}</strong> to <strong>{moment.utc(transaction.endDate).format('LL')}</strong></div>
-                                            <div className="price">${transaction.price}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                )
-                            }) : <p>No one has made reservations for your home yet.</p>}
+                            </div>
                         </div>
                     </div>
                 </div>
