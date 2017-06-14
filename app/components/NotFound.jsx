6c7c4f558fd668f145d0eaf592816592b7f3afbe
Rob Tong <robtong21@gmail.com>
2017-06-14 11:23:31 -0500

    fixed bug in main.jsx

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = props => {
   const userId = (props.user && props.user.id) || null
   console.error('NotFound: %s not found (%o)', pathname, props)
   return (
-    <div className="fourofour fullheight">
+    <div className="fourofour">
       <div className="container whitetext">
         <h1>Ever found yourself lost in space? Or like your request went in a black hole? Well, this is one of those times, unfortunately.</h1>
         <p><em>Did you spell your URL correctly? You entered <strong>{pathname}</strong></em></p>
